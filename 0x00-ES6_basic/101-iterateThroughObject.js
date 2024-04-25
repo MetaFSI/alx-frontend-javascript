6b82cb4a009d229c18a4bbee22fcb9be60171db4
Mounir el fahsi <elfahssimounir7@gmail.com>
2024-04-25 12:47:39 +0100

    fix

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,8 +1,8 @@
 export default function iterateThroughObject(reportWithIterator) {
-  // Utiliser la méthode Array.
+  // Utiliser méthode Array.from pour convertir l'itérateur en tableau
   const employeesArray = Array.from(reportWithIterator);
 
-  // Utiliser méthode 
+  // Utiliser méthode join pour concaténer les noms des employés avec '|'
   const result = employeesArray.join(' | ');
 
   return result;
